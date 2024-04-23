79b2343d3a483a1e996fe5b44ab3eebd063db981
Joseph Wabwile <126401697+1yama@users.noreply.github.com>
2024-04-23 07:52:39 +0300

    Update admin_credentials.py.py

--- admin_credentials.py.py
+++ admin_credentials.py.py
@@ -7,5 +7,5 @@ admin_credentials = {
     # Add more admin usernames and hashed passwords as needed
 }
 # Serialize and save the admin_credentials dictionary to a .pkl file
-with open("admin_credentials.pkl", "wb") as file:
+with open("admin_credentials.py", "wb") as file:
     pickle.dump(admin_credentials, file)
