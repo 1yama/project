1a165da268f0267fea95016b87847416c963a30f
Joseph Wabwile <126401697+1yama@users.noreply.github.com>
2024-04-23 10:06:09 +0300

    Update main_streamlit.py

--- main_streamlit.py
+++ main_streamlit.py
@@ -6,18 +6,6 @@ import pandas as pd
 import matplotlib as plt
 import seaborn as sns
 
-import pickle
-
-# Define the admin credentials as a dictionary
-admin_credentials = {
-    "Admin1": "hashed_password1",
-    "Admin2": "hashed_password2",
-    # Add more admin usernames and hashed passwords as needed
-}
-# Serialize and save the admin_credentials dictionary to a .pkl file
-with open("admin_credentials.pkl", "wb") as file:
-    pickle.dump(admin_credentials, file)
-
 
 # Model Building
 # imports for model algorithms and model evaluation
